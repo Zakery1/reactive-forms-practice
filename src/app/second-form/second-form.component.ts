f0cab1dfe71620a840da5ac12626da22906741b3
Zachary Graham <zakgraham@gmail.com>
2019-02-25 22:05:52 -0700

    finish reactive forms tutorial

--- src/app/second-form/second-form.component.ts
+++ src/app/second-form/second-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
 	selector: 'app-second-form',
@@ -8,16 +8,27 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class SecondFormComponent implements OnInit {
 
-	profileForm = new FormGroup({
-		firstName: new FormControl(''),
-		lastName: new FormControl(''),
-		address: new FormGroup({
-			street: new FormControl(''),
-			city: new FormControl(''),
-			state: new FormControl(''),
-			zip: new FormControl('')
-		})
+	profileForm = this.fb.group({
+		firstName: ['', Validators.required],
+		lastName: [''],
+		address: this.fb.group({
+			street: [''],
+			city: [''],
+			state: [''],
+			zip: ['']
+		}),
+		aliases: this.fb.array([
+			this.fb.control('')
+		])
 	});
+
+	get aliases() {
+		return this.profileForm.get('aliases') as FormArray;
+	}
+
+	addAlias() {
+		this.aliases.push(this.fb.control(''));
+	}
 
 	updateProfile() {
 		this.profileForm.patchValue({
@@ -32,7 +43,7 @@ export class SecondFormComponent implements OnInit {
 		console.warn(this.profileForm.value);
 	}
 
-	constructor() { }
+	constructor(private fb: FormBuilder) { }
 
 	ngOnInit() {
 	}
